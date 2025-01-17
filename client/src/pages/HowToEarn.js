0830f64a79e8912865b20d4158b327627463b159
AlexSec-DEV <alexsec0688@gmail.com>
2025-01-17 18:10:29 +0200

    Necə qazanırıq sayfasında Cloudinary resim URL'si güncellendi

--- client/src/pages/HowToEarn.js
+++ client/src/pages/HowToEarn.js
@@ -30,7 +30,7 @@ const HowToEarn = () => {
             minHeight: '80vh'
           }}>
             <img 
-              src="https://res.cloudinary.com/blackkey/image/upload/neceqazaniriq/ngnatk0ycsu4fjtjdxaz.png" 
+              src="https://res.cloudinary.com/dgrbjuqxl/image/upload/v1737128307/neceqazaniriq/ngnatk0ycsu4fjtjdxaz.png" 
               alt="Necə qazanırıq?"
               style={{
                 maxWidth: '100%',
